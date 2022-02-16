baf1af78cc7a64e5ba9c145dcd69af0fc245a889
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-02-16 14:36:15 +0100

    passed in data as props from App to Todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div></div>;
+import Todo from "./components/Todo";
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React"/>
+      <Todo text="Master React"/>
+      <Todo text="Expore the full React course"/>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+const Todo = (props) => {
+    return (
+        <div className='card'>
+                <h2>{props.text}</h2>
+                <div className='actions'>
+                    <button className='btn'>Delete</button>
+                </div>
+        </div>
+    )    
+}
+
+
+export default Todo;
